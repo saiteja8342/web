cf23756c393cfc058c4d8535bfaab9963439ee97
teja <saiteja8342@gmail.com>
2026-09-02 21:28:38 +0530

    this fila

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -81,13 +81,7 @@ export default function Navbar() {
               "rgb(255, 200, 100)",
             ]}
           >
-            <a 
-              href="/login.html" 
-              target="_blank" 
-              rel="noopener noreferrer" 
-              className="nav-cta-custom-noise" 
-              data-hover-type="link"
-            >
+            <a href={isSecondaryPage ? "/#contact" : "#contact"} className="nav-cta-custom-noise" data-hover-type="link">
               Start a project
             </a>
           </NoiseBackground>
